bf0c7ad5a95cca8915e8379ba2f1e427699a64e4
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-03-30 23:12:35 +0530

    Adding/Removing Friend Feature File with source code

---
+++ Add-Remove Friend Feature/script.js
@@ -0,0 +1,30 @@
+let stats = document.querySelector("h5")
+
+let add = document.querySelector("#add");
+
+
+click = 0;
+
+add.addEventListener("click",function(){
+  
+    stats.innerHTML = "Friends"
+    stats.style.color = "Green"
+    add.style.backgroundColor = "#4287f5"
+    // stats.style.backgroundColor = "#eeeee4"
+    remove.style.backgroundColor = '#e3d6d5';
+    
+    
+    
+})
+
+remove.addEventListener("click",function(){
+    stats.innerHTML = "Stranger"
+    stats.style.color = "Red"
+    stats.style.backgroundColor = "#ffff"
+    remove.style.backgroundColor = "#dadada"
+    add.style.backgroundColor = 'cadetblue'
+
+   
+    
+    
+})
