8585b9a8c46eaab10bf031fb02e7f54b55220710
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-04-01 20:19:13 +0530

    Insta Love Heart Feature using JavaScript

---
+++ Insta Love Button/script.js
@@ -0,0 +1,19 @@
+let container = document.querySelector("#container");
+
+let icon = document.querySelector("i");
+
+container.addEventListener("dblclick", function(){
+    icon.style.transform = 'translate(-50%,-50%) scale(1)'
+    icon.style.opacity = 0.8
+    icon.style.color = '#FE251B'
+
+    setTimeout(() => {
+        icon.style.opacity = 0;
+    }, 1000);
+
+
+    setTimeout(function(){
+        icon.style.transform = 'translate(-50%,-50%) scale(0)'
+    },2000)
+})
+
